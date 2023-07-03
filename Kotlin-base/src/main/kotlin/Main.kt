d9ec1380773fe119c87349a70aced14383456f93
Ilya <124510757+DevIsaev@users.noreply.github.com>
2023-07-03 23:47:35 +0700

    Массивы

---
+++ Kotlin-base/src/main/kotlin/Main.kt
@@ -0,0 +1,108 @@
+//глобальная переменная
+var str1="Hello"
+var num3=8
+//основная функция
+fun main(args: Array<String>) {
+
+    //
+    // println("printLN")
+    // print("Kotlin")
+    //
+
+
+    //
+    //типы переменных, локальные переменные
+    //var
+    // var age: Int=18
+    // age=19
+    // println("Мой возраст - $age")
+    // var name: String="Илья"
+    // var end:Char='s'
+    // var isTrue:Boolean=true
+    // var num:Float=4.5f
+    // var num2:Double = 4.556
+    // var null1: Int?=null
+    // var name2="Григорий"
+    // println(name+end)
+    // println(isTrue)
+    // println(num)
+    // println(num2)
+    // var res=num2+num3
+    // res+=10
+    // res--
+    // println(res)
+
+    //const(val)
+    // val number:Int=4
+    //
+
+
+    //
+    //условные операторы
+    //if else
+    /*
+    var str:String?=null
+    var isHas=true
+
+    str="Hello"
+    if(str==null) {
+        println("str пустое")
+
+        if (isHas==true) {
+            println("isHas = true")
+        }
+        else{
+            println("isHas=false")
+        }
+
+    }
+    else if(str=="Hello"){
+        println(str)
+    }
+    else{
+        println("str не пустое")
+    }
+
+    //when
+    val num=5
+    when(num){
+        null-> println("num = null")
+        10-> println(num)
+        5-> println(num)
+        else -> println("не то")
+    }
+
+    var res=if (num>30) 1 else 0
+    println(res)
+
+    var words=when(num){
+        null-> "null"
+        else->num
+    }
+    println(words)*/
+    //
+
+    //
+    //функции
+    /*fun1()
+    fun2("Test",1)
+    println(fun3(56,44))
+    println(fun4(true))*/
+    //
+
+    //
+    //массивы
+    
+    //
+}
+
+/*fun fun1(){
+    println("fun1")
+}
+fun fun2(first:String, num:Int){
+    println("$first $num")
+}
+fun fun3(x:Int,y:Int):Int{
+    return x+y
+}
+fun fun4(b:Boolean):String = "Fun4-$b"*/
