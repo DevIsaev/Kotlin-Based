f539dd4bc5db6bc3307f2ee3461b0402ac236231
DevIsaev <iisaev263@yandex.ru>
2023-07-31 15:08:01 +0700

    Курс Неко 2020 - Исправлен светофор

--- Neco_ru/02Swetofor/app/src/main/java/com/example/a02swetofor/MainActivity.kt
+++ Neco_ru/02Swetofor/app/src/main/java/com/example/a02swetofor/MainActivity.kt
@@ -3,55 +3,57 @@ package com.example.a02swetofor
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 import android.widget.ImageView
 import kotlinx.coroutines.delay
 import java.util.Timer
 import java.util.TimerTask
 
 class MainActivity : Activity() {
-    var timer=Timer()
-    var counter = 0
-    var imSwetofor:ImageView?=null
+    var timer:Timer?=null
     var arrColors:IntArray= intArrayOf(R.drawable.semafor_red,R.drawable.semafor_yellow,R.drawable.semafor_green)
+    var counter = 0
+    var isStart=false
+    var imSwetofor:ImageView?=null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         imSwetofor=findViewById(R.id.swetofor)
-        var button=findViewById<ImageView>(R.id.bGo)
-        var isRun=false
-
-
-
-
-        button.setOnClickListener {
-            if(!isRun){
-                StartTimer()
-                button.setImageResource(R.drawable.button_stop)
-                isRun=true
-            }
-            else{
-                button.setImageResource(R.drawable.button_start)
-                imSwetofor?.setImageResource(R.drawable.semafor_grey)
-                timer.cancel()
-                isRun=false
-                counter=0
-            }
+    }
+    fun onClickStartStop(view:View){
+        view as ImageButton
+        if (!isStart){
+            timer=Timer()
+            StartStop()
+            view.setImageResource(R.drawable.button_stop)
+            isStart=true
+        }
+        else{
+            imSwetofor?.setImageResource(R.drawable.semafor_grey)
+            view.setImageResource(R.drawable.button_start)
+            timer?.cancel()
+            isStart=false
+            counter=0
         }
 
     }
-    fun StartTimer(){
+    fun StartStop(){
+        timer=Timer()
         timer?.schedule(object :TimerTask(){
+
             override fun run() {
                 runOnUiThread {
                     imSwetofor?.setImageResource(arrColors[counter])
                     counter++
-                    if (counter == 2) {
-                        counter = 0
-                    }
+                    if (counter == 3)counter = 0
+
                 }
             }
 
-        },100)
+        },0,1000)
     }
 }
