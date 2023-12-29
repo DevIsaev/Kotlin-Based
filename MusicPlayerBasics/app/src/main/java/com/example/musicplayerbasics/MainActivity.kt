85a99c01c52cadfbf7f810338b69489eac3d31ff
DevIsaev <iisaev263@yandex.ru>
2023-12-29 22:43:37 +0700

    Реализованы ND и BottomSheet с плеером

--- MusicPlayerBasics/app/src/main/java/com/example/musicplayerbasics/MainActivity.kt
+++ MusicPlayerBasics/app/src/main/java/com/example/musicplayerbasics/MainActivity.kt
@@ -1,21 +1,30 @@
 package com.example.musicplayerbasics
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.musicplayerbasics.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding:ActivityMainBinding
+    var drawer: DrawerLayout? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestRuntimePermission()
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Navigation()
 
         binding.favouritesBTN.setOnClickListener {
-            val itent=Intent(this,FavouritesActivity::class.java)
-            startActivity(itent)
+            showBottomSheet()
         }
         binding.shuffleBTN.setOnClickListener {
             val itent=Intent(this,PlayerActivity::class.java)
@@ -25,5 +34,85 @@ class MainActivity : AppCompatActivity() {
             val itent=Intent(this,PlaylistsActivity::class.java)
             startActivity(itent)
         }
+    }
+    //запрос разрешения
+    private fun requestRuntimePermission(){
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            !=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),13)
+        }
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode==13){
+            if(grantResults.isNotEmpty()&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this,"Разрешение получено",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),13)
+            }
+        }
+    }
+    //Navigation drawer
+    private fun Navigation(){
+        var NavView=findViewById<NavigationView>(R.id.NavView)
+        NavView.setNavigationItemSelectedListener(this)
+        drawer = findViewById(R.id.Drawer)
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.IDSignOut -> {
+                Toast.makeText(this, "IDSignOut", Toast.LENGTH_SHORT).show()
+                showBottomSheet()
+            }
+
+            R.id.IDMusicStorage -> {
+                Toast.makeText(this, "IDMusicStorage", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.IDMusicDownload -> {
+                Toast.makeText(this, "IDMusicDownload", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.IDStore -> {
+                Toast.makeText(this, "IDStore", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.IDPlaylists -> {
+                Toast.makeText(this, "IDPlaylists", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.IDStatistic -> {
+                Toast.makeText(this, "IDStatistic", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.IDSettings -> {
+                Toast.makeText(this, "IDSettings", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.IDPlugIn -> {
+                Toast.makeText(this, "IDPlugIn", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.ID_F_A_Q -> {
+                Toast.makeText(this, "ID_F_A_Q", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.IDAbout -> {
+                Toast.makeText(this, "IDAbout", Toast.LENGTH_SHORT).show()
+            }
+        }
+        drawer?.closeDrawer(GravityCompat.START)
+        return true
+    }
+    //sheet
+    private fun showBottomSheet(){
+//       val dialogview=layoutInflater.inflate(R.layout.activity_player,null)
+//        dialog=BottomSheetDialog(this,R.style.BottomSheetTheme)
+//        dialog.setContentView(dialogview)
+//        dialog.show()
+
+    val bottomsheet=PlayerFragment()
+    bottomsheet.show(supportFragmentManager,"BottomSheetDialog")
     }
 }
--- MusicPlayerBasics/app/src/main/java/com/example/musicplayerbasics/PlayerActivity.kt
+++ MusicPlayerBasics/app/src/main/java/com/example/musicplayerbasics/PlayerActivity.kt
@@ -1,6 +1,7 @@
 package com.example.musicplayerbasics
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.musicplayerbasics.databinding.ActivityPlayerBinding
 
@@ -10,5 +11,8 @@ class PlayerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivityPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.pause.setOnClickListener {
+            Toast.makeText(this,"Pause",Toast.LENGTH_SHORT).show()
+        }
     }
 }
